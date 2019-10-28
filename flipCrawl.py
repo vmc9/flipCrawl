19a705ec73ed6923d5345b3549612e1e28e7fdf7
Victor <victor_mc@live.ca>
2019-10-28 17:10:09 -0400

    Update flipCrawl.py

--- flipCrawl.py
+++ flipCrawl.py
@@ -5,30 +5,7 @@ from bs4 import BeautifulSoup
 
 # Dictionary of Magazines
 magazines = {
-    "startups": "https://flipboard.com/@sconnexions/startups-6q42rjd9z",
-    "IoT": "https://flipboard.com/@sconnexions/internet-of-things-(iot)-08aq40osz",
-    "scienceEnvironment": "https://flipboard.com/@sconnexions/sciences-connexions---environnement-48cqponqz",
-    "blockchain": "https://flipboard.com/@sconnexions/blockchain-74gnm8gbz",
-    "ux": "https://flipboard.com/@sconnexions/ux-(user-experience)-pehj9g0hz",
-    "digitalTransformation": "https://flipboard.com/@sconnexions/digital-transformation-818s5iv1z",
-    "digitalContinuous": "https://flipboard.com/@sconnexions/digital-continuous-improvement-m1rikuphz",
-    "aeroMTL": "https://flipboard.com/@sconnexions/montreal%27s-aero-digital-ecosystem-126lovekz",
-    "wearables": "https://flipboard.com/@sconnexions/wearables-kabn6beqz",
-    "exoskeletons": "https://flipboard.com/@sconnexions/exoskeletons-bd6nr3nkz",
-    "additiveManufacturing": "https://flipboard.com/@sconnexions/additive-manufacturing-j9ustp35z",
-    "cybersecurity": "https://flipboard.com/@sconnexions/cybersecurity-lg789ov9z",
-    "ar": "https://flipboard.com/@sconnexions/augmented-reality-usv9e6vmz",
-    "plm": "https://flipboard.com/@sconnexions/product-lifecycle-management-(plm)-jdf1p3tvz",
-    "cloudComputing": "https://flipboard.com/@sconnexions/cloud-computing-obne0gjnz",
-    "data&analytics": "https://flipboard.com/@sconnexions/data-and-analytics-l4v7vm0jz",
-    "agile": "https://flipboard.com/@sconnexions/agile-ruld40jhz",
-    "web": "https://flipboard.com/@sconnexions/web-development-qcg9fmjdz",
-    "digitalTech": "https://flipboard.com/@sconnexions/digital-technology-watch-r7i52qs0z",
-    "ai": "https://flipboard.com/@sconnexions/artificial-intelligence-r7pcqfuqz",
-    "quantum": "https://flipboard.com/@sconnexions/quantum-computing-3g0n38msz",
-    "aeroTechnology": "https://flipboard.com/@sconnexions/aerospace-technology-watch-70m3do2dz",
-    "science": "https://flipboard.com/@sconnexions/sciences-connexions-pl0h8r52z",
-    "candidates": "https://flipboard.com/@sconnexions/candidates-for-digital-technology-watch-oe3lr4kaz"
+    # magazine name : magazine url
 }
 
 # Iteration over all Magazines
